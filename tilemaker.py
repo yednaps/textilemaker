c066ad48941b4121f4856af6158df3dae5a8031f
sp <yednap@gmail.com>
2015-08-18 15:55:50 -0700

    add ability to use randomly selected color theme

--- tilemaker.py
+++ tilemaker.py
@@ -1,15 +1,21 @@
 #!/usr/bin/python
 
-import PIL
 from PIL import ImageFont, ImageDraw, Image
 from random import randint
+import cPickle as pickle
 
 def maketile(txt="TEST",
              size=14,
-             tcol=tuple([randint(0,256) for i in range(3)]),
-             bcol=tuple([randint(0,256) for i in range(3)]),
+             col='rand',
+             tcol=(0,0,0),
+             bcol=(255,255,255),
+             fontfile='consolidago.ttf',
              outfile='test.png',
              direction='\\'):
+
+    if col == 'rand':
+        theme = random.choice(pickle.load(open('colors.p')))
+        bcol = theme[0]
 
     txtlength = len(txt)+1
 
@@ -29,6 +35,8 @@ def maketile(txt="TEST",
         dir = 1
 
     for i in range(txtlength):
+        if col == 'rand':
+            tcol = theme[1:][i%(len(theme)-1)]
         draw.text((0,(y + 1)*i),line[dir*i:]+line[:dir*i],tcol,font=font)
 
     img.save(outfile)
