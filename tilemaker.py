47ca3136843bd9029eab964ee0251ec2961bc965
sp <yednap@gmail.com>
2015-08-19 14:18:03 -0700

    add ability to select theme

--- tilemaker.py
+++ tilemaker.py
@@ -9,12 +9,17 @@ import cPickle as pickle
 @click.option('--tcol',default=(0,0,0),help='Text color')
 @click.option('--bcol',default=(255,255,255),help='Background color')
 @click.option('--col',is_flag=True,help='Random color option')
+@click.option('--palette',default=None,help='Select color theme')
 @click.option('--direction',default='\\',help='Pattern direction')
 @click.option('--outfile',default='test.png',help='Output file name')
-def maketile(txt,size,font,tcol,bcol,col,direction,outfile):
+def maketile(txt,size,font,tcol,bcol,col,palette,direction,outfile):
 
     if col:
-        theme = random.choice(pickle.load(open('colors.p')))
+        palette = random.randint(0,len(pickle.load(open('colors.p'))))
+
+    if palette:
+        print("Using theme {}".format(palette))
+        theme = {i:j for i,j in enumerate(pickle.load(open('colors.p')))}[int(palette)]
         bcol = theme[0]
 
     txtlength = len(txt)+1
@@ -35,7 +40,7 @@ def maketile(txt,size,font,tcol,bcol,col,direction,outfile):
         dir = 1
 
     for i in range(txtlength):
-        if col:
+        if palette:
             tcol = theme[1:][i%(len(theme)-1)]
         draw.text((0,(y + 1)*i),line[dir*i:]+line[:dir*i],tcol,font=font)
 
